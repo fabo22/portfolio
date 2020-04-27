addb7fa8ca6ee11c48feb83c5b6791b30932d3a3
fabo <lopezfabrizio@ymail.com>
2020-04-27 15:20:18 -0500

    work on contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 export default class Contact extends Component {
     render() {
         return(
-            <div>
-                <h1>Contact Page</h1>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Fabrizio Lopez</h2>
+                        <img
+                        src="https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?k=6&m=476085198&s=612x612&w=0&h=5cDQxXHFzgyz8qYeBQu2gCZq1_TN0z40e_8ayzne0X0="
+                        alt="avatar"
+                        style={{ height: '250px' }}
+                        />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>Feel free to contact me if you have any questions. Here are my main forms of contact, cell and e-mail. You can also contact me via LinkedIn if you prefer.</p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr />
+
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Cutive'}}>
+                                        <i className="fa fa-phone-square" aria-hidden="true"/>
+                                        (972) 567-8148
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Cutive'}}>
+                                        <i className="fa fa-envelope" aria-hidden="true"/>
+                                        lopezfabrizio@ymail.com
+                                    </ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+
+                    </Cell>
+                </Grid>
             </div>
         );
     }
